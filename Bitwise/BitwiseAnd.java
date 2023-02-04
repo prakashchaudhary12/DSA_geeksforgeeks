0be14546301cb62fce63b1a48b8bc9f6b011f8b2
Prakash Chaudhary <chyprakash2002@gmail.com>
2023-02-04 10:34:57 +0530

    Add files via upload

---
+++ Bitwise/BitwiseAnd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class BitwiseAnd {
+    public static void main(String []args){
+        int x = 3;
+        int y = 6;
+        System.out.println(x&y);
+    }
+    
+}
---
+++ Bitwise/LeftShift.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class LeftShift {
+    public static void main(String []args){
+        int y = 4;
+        System.out.println(y<<4);
+        int x = -1;
+        System.out.println(x<<1);
+        System.out.println(x<<2);
+    }
+    
+}
